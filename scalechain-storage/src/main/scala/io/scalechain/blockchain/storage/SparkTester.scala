8714ac7b207fe52f5b1f929f5c1fb1431a76a4ab
Kangmo Kim <kangmo.kim@gmail.com>
2016-03-07 21:15:37 +0900

    Update spart tester to set master

--- scalechain-storage/src/main/scala/io/scalechain/blockchain/storage/SparkTester.scala
+++ scalechain-storage/src/main/scala/io/scalechain/blockchain/storage/SparkTester.scala
@@ -4,8 +4,8 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 object SparkTester {
   def main(args: Array[String]) {
-    val logFile = "YOUR_SPARK_HOME/README.md" // Should be some file on your system
-    val conf = new SparkConf().setAppName("Simple Application")
+    val logFile = "/Users/kangmo/crypto/scalechain/README.md" // Should be some file on your system
+    val conf = new SparkConf().setMaster("local[2]").setAppName("Simple Application")
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
