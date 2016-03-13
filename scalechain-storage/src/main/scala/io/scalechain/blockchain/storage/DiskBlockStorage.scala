39f21926d561db030fc10b35ef23bc6c8d7e79bd
Kangmo Kim <kangmo.kim@gmail.com>
2016-03-13 21:40:26 +0900

    Update block file info when a block is stored.
    - Whenever a new block data is stored on the record storage, we need to update the block file info on the key/value database.
    - Also added a comment to the disk block storage so that livecoding.tv viewers can understand what the disk block storage means.
    Issue : #21

--- scalechain-storage/src/main/scala/io/scalechain/blockchain/storage/DiskBlockStorage.scala
+++ scalechain-storage/src/main/scala/io/scalechain/blockchain/storage/DiskBlockStorage.scala
@@ -11,6 +11,41 @@ import org.slf4j.LoggerFactory
 
 import scala.collection.mutable
 
+/** Stores block header, block, and transactions in the block.
+  *
+  * Blocks are stored in two cases.
+  *
+  * 1. During IBD(Initial block download) process
+  *   * We use headers-first approach, so we download all headers from other peers first.
+  *     These headers are kept in the key/value database first.
+  *   * After all headers are downloaded, we download block data from other peers.
+  *     When we store blocks, we store them on the record storage, which writes records on blkNNNNN.dat file.
+  *   * After the block data is stored, we update the block info on the key/value database
+  *     to point to the record locator on the record storage.
+  *
+  * 2. After IBD process, we receive a block per (about) 10 minutes
+  *    In this case, both header and block data comes together.
+  *    We put both block and block header at once.
+  *
+  *
+  * Upon receival of blocks, we maintain the following indexes.
+  * keys and values are stored on the key/value database, whereas records are stored on the record storage.
+  *
+  * 1. (key) block hash -> (value) (block info) record locator -> (record) block data
+  * 2. (key) transaction hash -> (value) record locator -> (record) a transaction in the block data.
+  * 3. (key) file number -> (value) block file info
+  * 4. (key) static -> (value) best block hash
+  * 5. (key) static -> (value) last block file number
+  *
+  * How block headers and blocks are stored :
+  *
+  * 1. Only blockheader is stored -> A block data is stored. (OK)
+  * 2. A block is stored with block header at once. (OK)
+  * 3. A block is stored twice. => The second block data is ignored. A warning message is logged.
+  * 4. A blockheader is stored twice. => The second header data is ignored. A warning message is logged.
+  *
+  * @param directoryPath The path where database files are located.
+  */
 class DiskBlockStorage(directoryPath : File) extends BlockIndex {
   private val logger = LoggerFactory.getLogger(classOf[DiskBlockStorage])
 
@@ -25,6 +60,39 @@ class DiskBlockStorage(directoryPath : File) extends BlockIndex {
       bestBlockHeightOption = Some(height)
       blockIndex.putBestBlockHash(blockHash)
     }
+  }
+  protected[storage] def updateFileInfo(headerLocator : FileRecordLocator, blockHeight : Int, blockTimestamp : Long): Unit = {
+    val lastFileNumber = FileNumber(headerLocator.fileIndex)
+
+    // Are we writing at the beginning of the file?
+    // If yes, we need to update the last block file, because it means we created a file now.
+    if (headerLocator.recordLocator.offset == 0) {
+      blockIndex.putLastBlockFile(lastFileNumber)
+    }
+
+    // Update the block info.
+    val blockFileInfo = blockIndex.getBlockFileInfo(lastFileNumber).getOrElse(
+      BlockFileInfo(
+        blockCount = 0,
+        fileSize = 0L,
+        firstBlockHeight = blockHeight,
+        lastBlockHeight = blockHeight,
+        firstBlockTimestamp = blockTimestamp,
+        lastBlockTimestamp = blockTimestamp
+      )
+    )
+
+    // TODO : Need to make sure if it is ok even though a non-best block decreases the lastBlockHeight.
+    // Is the lastBlockHeight actually meaning maximum block height?
+    blockIndex.putBlockFileInfo(
+      lastFileNumber,
+      blockFileInfo.copy(
+        blockCount = blockFileInfo.blockCount + 1,
+        fileSize = blockRecordStorage.lastFile.size,
+        lastBlockHeight = blockHeight,
+        lastBlockTimestamp = blockTimestamp
+      )
+    )
   }
 
   def putBlock(block : Block) : Unit = {
@@ -48,6 +116,8 @@ class DiskBlockStorage(directoryPath : File) extends BlockIndex {
         val appendResult = blockWriter.appendBlock(block)
         val newBlockInfo= blockInfo.get.copy(blockLocatorOption = Some(appendResult.headerLocator))
         blockIndex.putBlockInfo(blockHash, newBlockInfo)
+        updateFileInfo(appendResult.headerLocator, newBlockInfo.height, block.header.timestamp)
+
         appendResult.txLocators
       } else {
         // The block already exists. Do not put it once more.
@@ -71,7 +141,9 @@ class DiskBlockStorage(directoryPath : File) extends BlockIndex {
           Some(appendResult.headerLocator)
         )
         blockIndex.putBlockInfo(blockHash, blockInfo)
+        updateFileInfo(appendResult.headerLocator, blockInfo.height, block.header.timestamp)
         checkBestBlockHash(blockHash, blockHeight)
+
         appendResult.txLocators
       } else {
         logger.warn("An orphan block was discarded while saving a block. block hash : {}", blockHash)
@@ -111,10 +183,13 @@ class DiskBlockStorage(directoryPath : File) extends BlockIndex {
         blockIndex.putBlockInfo(blockHash, blockInfo)
         checkBestBlockHash(blockHash, blockHeight)
       } else {
+        logger.warn("A block header is put onto the block database twice. block hash : {}", blockHash)
+
         // blockIndex hits an assertion if the block header is changed for the same block hash.
-        blockIndex.putBlockInfo(blockHash, blockInfo.get.copy(
-          blockHeader = blockHeader
-        ))
+        // TODO : Need to change to throw an exception if we try to overwrite with a different block header.
+        //blockIndex.putBlockInfo(blockHash, blockInfo.get.copy(
+        //  blockHeader = blockHeader
+        //))
       }
     } else {
       logger.warn("An orphan block was discarded while saving a block header. block hash : {}", blockHash)
