481f2a45a8d1f0ada07d381578cb57f764dc5e66
mjpark03 <mjpark03@gmail.com>
2016-03-23 17:08:10 +0900

    Prototype column family of rocksdb
    - why ? need to manage efficiently wallet data
    
    - how ?
    1) implement rocksdb using column family
    2) check test cases (RocksDatabaseCFSpec)
    
      "openWithColumnFamily" should "return an instance of RocksDB"
      "createColumnFamily" should "add new column family to existing column family list"
      "createColumnFamily" should "hit an assertion if new column family is already exist"
      "createColumnFamily" should "add more than 1,000,000 column family" --> it takes about 5minutes
      "dropColumnFamily" should "drop the column family"
      "putObject(column family, key, value)/getObject(column family, key)" should "store data and get data"
      "putObject(column family, key, value)" should "overwrite an existing data"
      "getObject(column family, key)" should "return None if the key is not exist"

--- scalechain-storage/src/main/scala/io/scalechain/blockchain/storage/index/RocksDatabase.scala
+++ scalechain-storage/src/main/scala/io/scalechain/blockchain/storage/index/RocksDatabase.scala
@@ -1,9 +1,9 @@
 package io.scalechain.blockchain.storage.index
 
 import java.io.File
-
+import java.util
 import io.scalechain.blockchain.storage.Storage
-import org.rocksdb.{Options, RocksDB};
+import org.rocksdb._
 
 /**
   * Created by kangmo on 3/11/16.
@@ -14,7 +14,62 @@ class RocksDatabase(path : File) extends KeyValueDatabase {
   // the Options class contains a set of configurable DB options
   // that determines the behavior of a database.
   private val options = new Options().setCreateIfMissing(true);
-  private val db = RocksDB.open(options, path.getAbsolutePath)
+  private var db = RocksDB.open(options, path.getAbsolutePath)
+
+  private val columnFamilyDescriptors = util.Arrays.asList(
+    new ColumnFamilyDescriptor(RocksDB.DEFAULT_COLUMN_FAMILY, new ColumnFamilyOptions())
+  )
+  private val columnFamilyHandles = new util.ArrayList[ColumnFamilyHandle]()
+
+  def openWithColumnFamily = {
+
+    // first, close db instance if an instance is already exist
+    close()
+    db = RocksDB.open(new DBOptions(), path.getAbsolutePath, columnFamilyDescriptors, columnFamilyHandles)
+  }
+
+  def createColumnFamily(descriptor: String) = {
+
+    // create new column family descriptor and new column family handler
+    val columnFamilyHandle = db.createColumnFamily(
+      new ColumnFamilyDescriptor(descriptor.getBytes(),
+      new ColumnFamilyOptions())
+    )
+
+    columnFamilyHandles.add(columnFamilyHandle)
+  }
+
+  def listColumnFamilies(): util.List[Array[Byte]] = {
+
+    // listColumnFamilies is static function
+    val columnFamilyNames = RocksDB.listColumnFamilies(options, path.getAbsolutePath)
+    columnFamilyNames
+  }
+
+  def dropColumnFamily(index: Int) = {
+
+    db.dropColumnFamily(columnFamilyHandles.get(index))
+  }
+
+  def getColumnFamilyIndex(descriptor: String): Int = {
+
+    val columnFamilyNames = listColumnFamilies()
+    var index = -1
+
+    for(i <- 0 until columnFamilyNames.size(); if index == -1) {
+      if(descriptor.equals( new String(columnFamilyNames.get(i)))) {
+        index = i
+      }
+    }
+
+    index
+  }
+
+  def getColumnFamilyHandle(descriptor: String): ColumnFamilyHandle = {
+
+    val index = getColumnFamilyIndex(descriptor)
+    columnFamilyHandles.get(index)
+  }
 
   def get(key : Array[Byte] ) : Option[Array[Byte]] = {
     val value = db.get(key)
@@ -23,8 +78,19 @@ class RocksDatabase(path : File) extends KeyValueDatabase {
     else None
   }
 
+  def get(cf: ColumnFamilyHandle, key: Array[Byte]): Option[Array[Byte]] = {
+    val value = db.get(cf, key)
+    if ( value != null )
+      Some(value)
+    else None
+  }
+
   def put(key : Array[Byte], value : Array[Byte] ) : Unit = {
     db.put(key, value)
+  }
+
+  def put(cf: ColumnFamilyHandle, key: Array[Byte], value: Array[Byte]) : Unit = {
+    db.put(cf, key, value)
   }
 
   def del(key : Array[Byte]) : Unit = {
--- scalechain-storage/src/test/scala/io/scalechain/blockchain/storage/index/RocksDatabaseCFSpec.scala
+++ scalechain-storage/src/test/scala/io/scalechain/blockchain/storage/index/RocksDatabaseCFSpec.scala
@@ -4,6 +4,7 @@ import java.io.File
 
 import io.scalechain.blockchain.storage.Storage
 import org.apache.commons.io.FileUtils
+import org.rocksdb.{RocksDBException}
 import org.scalatest._
 
 /**
@@ -31,40 +32,125 @@ class RocksDatabaseCFSpec extends FlatSpec with BeforeAndAfterEach with ShouldMa
     db.close()
   }
 
+  def L(arrayOption : Option[Array[Byte]]) =
+    arrayOption.map(_.toList)
+
   "openWithColumnFamily" should "return an instance of RocksDB" in {
+    db.openWithColumnFamily
 
-  }
-
-  "openWithColumnFamily" should "hit an assertion if an instance is already allocated" in {
-
+    db shouldBe a[RocksDatabase]
   }
 
   "createColumnFamily" should "add new column family to existing column family list" in {
+    db.openWithColumnFamily
 
+    // list has default column family
+    db.listColumnFamilies().size() shouldBe 1
+
+    val address1 = "12n9PRqdYQp9DPGV9yghbJHsoMZcd5xcum"
+    val address2 = "1N51L9K3G6TjQFpf4RChxho5qXsUMvbYhf"
+
+    db.createColumnFamily(address1)
+    db.createColumnFamily(address2)
+
+    val columnFamilyNames = db.listColumnFamilies()
+
+    columnFamilyNames.size() shouldBe 3
+    new String(columnFamilyNames.get(1)) shouldBe address1
+    new String(columnFamilyNames.get(2)) shouldBe address2
   }
 
   "createColumnFamily" should "hit an assertion if new column family is already exist" in {
+    db.openWithColumnFamily
 
+    val address = "12n9PRqdYQp9DPGV9yghbJHsoMZcd5xcum"
+
+    intercept[RocksDBException] {
+      db.createColumnFamily(address)
+      db.createColumnFamily(address)
+    }
   }
 
   "createColumnFamily" should "add more than 1,000,000 column family" in {
+    db.openWithColumnFamily
 
+    val max = 1000000
+    val address = "12n9PRqdYQp9DPGV9yghbJHsoMZcd5xcum"
+
+    for( i <- 1 to max){
+      db.createColumnFamily(address + i)
+    }
+
+    val columnFamilyNames = db.listColumnFamilies()
+    columnFamilyNames.size() shouldBe max+1
   }
 
   "dropColumnFamily" should "drop the column family" in {
+    db.openWithColumnFamily
 
+    val address1 = "12n9PRqdYQp9DPGV9yghbJHsoMZcd5xcum"
+    val address2 = "1N51L9K3G6TjQFpf4RChxho5qXsUMvbYhf"
+    val address3 = "1N51L9K3G6TjQFpf4RChxho5qXsUMvbYh3"
+
+    db.createColumnFamily(address1)
+    db.createColumnFamily(address2)
+    db.createColumnFamily(address3)
+
+    val beforeColumnFamilyNames = db.listColumnFamilies()
+    beforeColumnFamilyNames.size() shouldBe 4
+
+    val index = db.getColumnFamilyIndex(address1)
+    db.dropColumnFamily(index)
+
+    val afterColumnFamilyNames = db.listColumnFamilies()
+    afterColumnFamilyNames.size() shouldBe 3
   }
 
-  "putObject(column family, key, value)" should "store data" in {
+  "putObject(column family, key, value)/getObject(column family, key)" should "store data and get data" in {
+    db.openWithColumnFamily
 
+    val address1 = "12n9PRqdYQp9DPGV9yghbJHsoMZcd5xcum"
+    db.createColumnFamily(address1)
+
+    val columnFamilyHandle = db.getColumnFamilyHandle(address1)
+    val key = "test account key"
+    val value = "test account value"
+
+    db.put(columnFamilyHandle, key.getBytes(), value.getBytes())
+    L(db.get(columnFamilyHandle, key.getBytes())) shouldBe L(Some(value.getBytes()))
   }
 
   "putObject(column family, key, value)" should "overwrite an existing data" in {
+    db.openWithColumnFamily
 
+    val address1 = "12n9PRqdYQp9DPGV9yghbJHsoMZcd5xcum"
+    db.createColumnFamily(address1)
+
+    val columnFamilyHandle = db.getColumnFamilyHandle(address1)
+    val key = "test account key"
+    val value1 = "test account value1"
+    val value2 = "test account value2"
+
+    db.put(columnFamilyHandle, key.getBytes(), value1.getBytes())
+    L(db.get(columnFamilyHandle, key.getBytes())) shouldBe L(Some(value1.getBytes()))
+
+    db.put(columnFamilyHandle, key.getBytes(), value2.getBytes())
+    L(db.get(columnFamilyHandle, key.getBytes())) shouldBe L(Some(value2.getBytes()))
   }
 
-  "getObject(column family, key)" should "return a value" in {
+  "getObject(column family, key)" should "return None if the key is not exist" in {
+    db.openWithColumnFamily
 
+    val address1 = "12n9PRqdYQp9DPGV9yghbJHsoMZcd5xcum"
+    db.createColumnFamily(address1)
+
+    val columnFamilyHandle = db.getColumnFamilyHandle(address1)
+    val key1 = "test account key1"
+    val key2 = "test account key2"
+    val value = "test account value"
+
+    db.put(columnFamilyHandle, key1.getBytes(), value.getBytes())
+    db.get(columnFamilyHandle, key2.getBytes()) shouldBe None
   }
 
 }
