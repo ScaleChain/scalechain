3d2d272f7b6e5f2798fbe5613e0f80573840125e
Kangmo Kim <kangmo.kim@gmail.com>
2016-10-31 22:27:31 +0900

    Fix problem : PeerIndexCalculatorSpec fails Problem - getPeerIndex fails because peers does not exist any more. Root cause : After reducing the number of peers from 5 to 1, getPeerIndex(7644) fails, as there is no peer with the port 7644. Solution : Modify the test case to return None for the port 7644.

--- scalechain-net/src/test/scala/io/scalechain/blockchain/net/PeerIndexCalculatorSpec.scala
+++ scalechain-net/src/test/scala/io/scalechain/blockchain/net/PeerIndexCalculatorSpec.scala
@@ -79,11 +79,14 @@ class PeerIndexCalculatorSpec extends FlatSpec with BeforeAndAfterEach with Shou
   "getPeerIndex" should "return None if port does not match" in {
     PeerIndexCalculator.getPeerIndex(7642) shouldBe None
     PeerIndexCalculator.getPeerIndex(7643) shouldBe Some(0)
+    PeerIndexCalculator.getPeerIndex(7644) shouldBe None
+    /*
     PeerIndexCalculator.getPeerIndex(7644) shouldBe Some(1)
     PeerIndexCalculator.getPeerIndex(7645) shouldBe Some(2)
     PeerIndexCalculator.getPeerIndex(7646) shouldBe Some(3)
     PeerIndexCalculator.getPeerIndex(7647) shouldBe Some(4)
     PeerIndexCalculator.getPeerIndex(7648) shouldBe None
+    */
   }
 
 }
