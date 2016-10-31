f3afd54444f60e90ee92aa7e15d2938a3059f653
Kangmo Kim <kangmo.kim@gmail.com>
2016-10-31 15:30:36 +0900

    Change configuration files to start only one node. Why? Running five nodes for each test requires too much effort. How? Change configuration files under config folder.

--- run.sh
+++ run.sh
@@ -26,6 +26,6 @@ rm $LOG_FILE
 rm $EXCEPTION_FILE
 rm config/currentView 
 
-SBT_OPTS="$JAVA_OPTIONS" sbt "project scalechain-cli" "run-main io.scalechain.blockchain.cli.ScaleChainPeer -p $P2P_PORT -c $RPC_PORT --minerInitialDelayMS $MINER_INITIAL_DELAY_MS --minerHashDelayMS $MINER_HASH_DELAY_MS --cassandraAddress $CASSANDRA_IP" | tee $LOG_FILE | grep "Exception\|AssertionError\|NodeServerHandler\|NodeClientHandler\|ERROR\|WANR" | grep -v "Connection accepted from" | grep -v "Failed to connect" | tee $EXCEPTION_FILE 2>&1
-#SBT_OPTS="$JAVA_OPTIONS" sbt "project scalechain-cli" "run-main io.scalechain.blockchain.cli.ScaleChainPeer -p $P2P_PORT -c $RPC_PORT --minerInitialDelayMS $MINER_INITIAL_DELAY_MS --minerHashDelayMS $MINER_HASH_DELAY_MS --cassandraAddress $CASSANDRA_IP" | tee $LOG_FILE 
+#SBT_OPTS="$JAVA_OPTIONS" sbt "project scalechain-cli" "run-main io.scalechain.blockchain.cli.ScaleChainPeer -p $P2P_PORT -c $RPC_PORT --minerInitialDelayMS $MINER_INITIAL_DELAY_MS --minerHashDelayMS $MINER_HASH_DELAY_MS --cassandraAddress $CASSANDRA_IP" | tee $LOG_FILE | grep "Exception\|AssertionError\|NodeServerHandler\|NodeClientHandler\|ERROR\|WANR" | grep -v "Connection accepted from" | grep -v "Failed to connect" | tee $EXCEPTION_FILE 2>&1
+SBT_OPTS="$JAVA_OPTIONS" sbt "project scalechain-cli" "run-main io.scalechain.blockchain.cli.ScaleChainPeer -p $P2P_PORT -c $RPC_PORT --minerInitialDelayMS $MINER_INITIAL_DELAY_MS --minerHashDelayMS $MINER_HASH_DELAY_MS --cassandraAddress $CASSANDRA_IP" | tee $LOG_FILE 
 
