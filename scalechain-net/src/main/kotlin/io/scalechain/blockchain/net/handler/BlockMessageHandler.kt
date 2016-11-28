db125dfb44dc18b076b3a416d10f8655e64164e9
Kangmo Kim <kangmo.kim@gmail.com>
2016-11-28 10:46:37 +0900

    Added net layer to the build system.
    Why? to start compiling files in the net layer
    How? Create gradle files for compilation.

--- scalechain-net/src/main/kotlin/io/scalechain/blockchain/net/handler/BlockMessageHandler.kt
+++ scalechain-net/src/main/kotlin/io/scalechain/blockchain/net/handler/BlockMessageHandler.kt
@@ -2,7 +2,7 @@ package io.scalechain.blockchain.net.handler
 
 import com.typesafe.scalalogging.Logger
 import io.scalechain.blockchain.net.BlockGateway
-io.scalechain.blockchain.net.TimeBasedCache
+import io.scalechain.blockchain.net.TimeBasedCache
 import io.scalechain.blockchain.chain.Blockchain
 import io.scalechain.blockchain.chain.processor.BlockProcessor
 import io.scalechain.blockchain.net.BlockGateway
