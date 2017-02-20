d511e584c5f1398faefa8281193001f5b55f716d
Kevin <kevin@scalechain.io>
2017-02-20 18:42:47 +0900

    Add GetBlockCount RpcCommand

---
+++ scalechain-api/src/main/kotlin/io/scalechain/blockchain/api/command/blockchain/GetBlockCount.kt
@@ -0,0 +1,34 @@
+package io.scalechain.blockchain.api.command.blockchain
+
+import io.scalechain.blockchain.api.RpcSubSystem
+import io.scalechain.blockchain.api.command.RpcCommand
+import io.scalechain.blockchain.api.domain.NumberResult
+import io.scalechain.blockchain.api.domain.RpcError
+import io.scalechain.util.Either
+import io.scalechain.util.Either.Right
+import io.scalechain.blockchain.api.domain.RpcRequest
+import io.scalechain.blockchain.api.domain.RpcResult
+import java.math.BigDecimal
+
+object GetBlockCount : RpcCommand() {
+  override fun invoke(request: RpcRequest): Either<RpcError, RpcResult?> {
+    return handlingException {
+      val blockCount: Long = RpcSubSystem.get().getBestBlockHeight()
+      Right(NumberResult(BigDecimal.valueOf(blockCount)))
+    }
+  }
+
+  override fun help() : String =
+    """getblockcount
+      |
+      |Returns the number of blocks in the local best block chain.
+      |
+      |Result
+      |(int)    The number of blocks in the local best block chain.
+      |
+      |Examples:
+      |> bitcoin-cli -testnet getblockcount
+      |> curl --data-binary '{"jsonrpc": "1.0", "id":1, "method": "getblockcount", "params": [] }' -H
+      'Content-Type: application/json' http://127.0.0.1:8332/
+    """.trimMargin()
+}
