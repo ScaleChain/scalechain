8346132b138061c00af2a205ac5b01c9a75421bf
Kevin <kevin@scalechain.io>
2017-02-28 21:40:07 +0900

    Add more test cases to GetAddressesByAccountSpec

--- scalechain-cli/src/test/kotlin/io/scalechain/blockchain/cli/wallet/GetAddressesByAccountSpec.kt
+++ scalechain-cli/src/test/kotlin/io/scalechain/blockchain/cli/wallet/GetAddressesByAccountSpec.kt
@@ -3,10 +3,11 @@ package io.scalechain.blockchain.cli.wallet
 import com.google.gson.JsonPrimitive
 import io.kotlintest.KTestJUnitRunner
 import io.scalechain.blockchain.api.command.wallet.GetAddressesByAccount
+import io.scalechain.blockchain.api.domain.RpcError
 import io.scalechain.blockchain.api.domain.StringListResult
 import io.scalechain.blockchain.cli.APITestSuite
-import io.scalechain.blockchain.transaction.OutputOwnership
 import org.junit.runner.RunWith
+import kotlin.test.assertTrue
 
 @RunWith(KTestJUnitRunner::class)
 class GetAddressesByAccountSpec : APITestSuite() {
@@ -19,8 +20,20 @@ class GetAddressesByAccountSpec : APITestSuite() {
   }
 
   init {
-    val accounts = listOf("Account1", "Account2", "Account3")
-    "GetAccountAddress() for each account" should "return empty list each" {
+    val defaultAccount = AccountNameSample.default()
+    val account1 = AccountNameSample.random()
+    val account2 = AccountNameSample.random()
+
+    val accounts = listOf(defaultAccount, account1, account2)
+
+    "GetAddressesByAccount" should "return RPC_INVALID_REQUEST when no account parameter" {
+      val result = invoke(GetAddressesByAccount, listOf())
+      val error = result.left()!!
+
+      error.code shouldBe RpcError.RPC_INVALID_REQUEST.code
+    }
+
+    "GetAddressesByAccount" should "return empty list each for each account" {
       accounts.forEach {
         val result = invoke(GetAddressesByAccount, listOf(JsonPrimitive(it)))
         val addresses = (result.right()!! as StringListResult).value
@@ -29,27 +42,50 @@ class GetAddressesByAccountSpec : APITestSuite() {
       }
     }
 
-    "GetAccountAddress() for each account" should "return one address each" {
-      val accountAddressMap = mapOf<String, OutputOwnership>(
-        Pair(accounts[0], Data.Addr1.address),
-        Pair(accounts[1], Data.Addr2.address),
-        Pair(accounts[2], Data.Addr3.address)
-      )
+    "GetAddressesByAccount" should "return one address for ''(default account)" {
+      val address = Data.Addr1.address
 
-      wallet.importOutputOwnership(chain.db, chain, accounts[0], accountAddressMap[accounts[0]]!!, false)
-      wallet.importOutputOwnership(chain.db, chain, accounts[1], accountAddressMap[accounts[1]]!!, false)
-      wallet.importOutputOwnership(chain.db, chain, accounts[2], accountAddressMap[accounts[2]]!!, false)
+      wallet.importOutputOwnership(chain.db, chain, defaultAccount, address, false)
 
-      accountAddressMap.entries.forEach {
-        val account = it.key
-        val address = it.value.stringKey()
+      val result = invoke(GetAddressesByAccount, listOf(JsonPrimitive(defaultAccount)))
+      val addresses = (result.right()!! as StringListResult).value
 
-        val result = invoke(GetAddressesByAccount, listOf(JsonPrimitive(account)))
-        val addresses = (result.right()!! as StringListResult).value
+      addresses should haveSize(1)
+      addresses[0] shouldBe address.stringKey()
+    }
 
-        addresses should haveSize(1)
-        addresses[0] shouldBe address
+    "GetAddressesByAccount" should "return two address for account1" {
+      val address1 = Data.Addr1.address
+      val address2 = Data.Addr2.address
+
+      wallet.importOutputOwnership(chain.db, chain, account1, address1, false)
+      wallet.importOutputOwnership(chain.db, chain, account1, address2, false)
+
+      val result = invoke(GetAddressesByAccount, listOf(JsonPrimitive(account1)))
+      val addresses = (result.right()!! as StringListResult).value
+
+      addresses should haveSize(2)
+      assertTrue(addresses.contains(address1.stringKey()))
+      assertTrue(addresses.contains(address2.stringKey()))
+    }
+
+    "GetAddressesByAccount" should "return three address for account2" {
+      val address1 = Data.Addr1.address
+      val address2 = Data.Addr2.address
+      val address3 = Data.Addr3.address
+
+      listOf(address1, address2, address3).forEach {
+        wallet.importOutputOwnership(chain.db, chain, account2, it, false)
+      }
+
+      val result = invoke(GetAddressesByAccount, listOf(JsonPrimitive(account2)))
+      val addresses = (result.right()!! as StringListResult).value
+
+      addresses should haveSize(3)
+      listOf(address1, address2, address3).forEach {
+        assertTrue(addresses.contains(it.stringKey()))
       }
     }
   }
 }
+
